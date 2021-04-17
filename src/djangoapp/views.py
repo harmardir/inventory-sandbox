e2f726ddd73577a776fe45abb373ee9da5ad4a2c
Harout <harout-dev@gmail.com>
2021-04-17 13:12:17 +0300

    Adding data using front-end

--- src/djangoapp/views.py
+++ src/djangoapp/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 
+from .forms import ComputerForm
+
+
 def home(request):
 	title = 'Welcome: This is the Home Page'
 	context = {"title": title,}
 	return render(request, "home.html",context)
+
+
+def computer_entry(request):
+    title = 'Add Computer'
+    form = ComputerForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+    context = {"title": title,"form": form,}
+    return render(request, "computer_entry.html",context)
--- src/djangoproject/urls.py
+++ src/djangoproject/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.conf.urls import include, url
 from django.urls import path
-from djangoapp.views import home
+from djangoapp.views import home , computer_entry
 
 urlpatterns = [
     url('admin/',admin.site.urls),
     url(r'^$', home, name='home'),
+    url(r'^computer_entry/$',computer_entry, name='computer_entry'),
 ]
