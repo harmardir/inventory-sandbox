0bd4fb387f15677880fc959d7bbdb0db6223bfaf
Harout <harout-dev@gmail.com>
2021-04-17 11:42:30 +0300

    Creating forms

---
+++ src/djangoapp/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Computer
+
+
+class ComputerForm(forms.ModelForm):
+    class Meta:
+        model = Computer
+        fields = ['computer_name', 'IP_address', 'MAC_address', 'users_name', 'location']
