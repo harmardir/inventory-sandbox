f2c65738463f95ae067f69b885e9cd5d9fb47cd0
Harout <harout-dev@gmail.com>
2021-04-18 22:11:27 +0300

    Adding delete button

--- src/djangoproject/urls.py
+++ src/djangoproject/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.conf.urls import include, url
 from django.urls import path
-from djangoapp.views import home , computer_entry , computer_list , computer_edit
+from djangoapp.views import home , computer_entry , computer_list , computer_edit , computer_delete
 
 urlpatterns = [
     url('admin/',admin.site.urls),
@@ -9,4 +9,5 @@ urlpatterns = [
     url(r'^computer_entry/$',computer_entry, name='computer_entry'),
     url(r'^computer_list/$', computer_list, name='computer_list'),
     url(r'^computer_list/(?P<id>\d+)/$', computer_edit, name='computer_edit'),
+    url(r'^computer_list/(?P<id>\d+)/delete$',computer_delete, name='computer_delete'),
 ]
