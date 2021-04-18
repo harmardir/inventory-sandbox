974d0b14847c6c47eb50e8ae9526e02778f23e1c
Harout <harout-dev@gmail.com>
2021-04-19 00:08:29 +0300

    Adding form validation

--- src/djangoapp/forms.py
+++ src/djangoapp/forms.py
@@ -7,6 +7,21 @@ class ComputerForm(forms.ModelForm):
         model = Computer
         fields = ['computer_name', 'IP_address', 'MAC_address', 'users_name', 'location', 'purchase_date']
 
+    def clean_computer_name(self): # Validates the Computer Name Field
+        computer_name = self.cleaned_data.get('computer_name')
+        if (computer_name == ""):
+            raise forms.ValidationError('This field cannot be left blank')
+        for instance in Computer.objects.all():
+            if instance.computer_name == computer_name:
+                raise forms.ValidationError(computer_name + ' is already added')
+        return computer_name
+
+    def clean_IP_address(self): # Validates the Computer Name Field
+        IP_address = self.cleaned_data.get('IP_address')
+        if (IP_address == ""):
+            raise forms.ValidationError('This field cannot be left blank')
+        return IP_address
+
 class ComputerSearchForm(forms.ModelForm):
     class Meta:
         model = Computer
