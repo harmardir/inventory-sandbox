5f2f9607b20e7421ddb6a3e915ae29c9b9fd9921
Harout <harout-dev@gmail.com>
2021-04-18 20:05:02 +0300

    Adding crispy forms

--- src/djangoproject/settings.py
+++ src/djangoproject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'djangoapp',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -119,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
